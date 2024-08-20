740b50305ef8251fe1fe2529eb904b9ee2589f2a
FMScripts <166993435+FMscripts1@users.noreply.github.com>
2024-08-20 19:26:30 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+author 'FMScripts'
+version '1.0'
+fx_version 'cerulean'
+game 'gta5'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+ui_page 'html/index.html'
+
+files {
+    -- ESSENTIALS
+    'html/coin.png',
+    'html/scratch.mp3',
+    -- UI
+    'html/index.html',
+    'html/script.js',
+    -- Connection With LUA
+    'html/main.js',
+    -- images
+    'images/*'
+}
